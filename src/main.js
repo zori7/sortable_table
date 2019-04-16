9c2127ce72b847e658efc4a6627f9aa2de3bb548
Zori Yeghikyan <zori.egikyan@gmail.com>
2019-04-16 14:29:41 +0400

    v1.1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue';
-import Paginate from 'vuejs-paginate';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 import App from './App.vue';
 
 Vue.config.productionTip = false;
-Vue.component('paginate', Paginate);
 Vue.use(ElementUI);
 
 new Vue({
